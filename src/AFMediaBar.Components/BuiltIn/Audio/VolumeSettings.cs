ab078a3f602ce74f38589f599f196bc9843fe1cf
jiang miu miu <qewr367@yeah.net>
2026-08-28 15:38:30 +0800

    Remove: src/AFMediaBar.Components/BuiltIn/Audio/VolumeSettings.cs

--- src/AFMediaBar.Components/BuiltIn/Audio/VolumeSettings.cs
+++
@@ -1,8 +0,0 @@
-using AFMediaBar.Components.Abstractions;
-
-namespace AFMediaBar.Components.BuiltIn.Audio;
-
-public sealed record VolumeSettings(int ButtonSizeDip = 24, int WheelStepPercent = 2) : IComponentSettings
-{
-    public string TypeId => ComponentTypeIds.Volume;
-}
