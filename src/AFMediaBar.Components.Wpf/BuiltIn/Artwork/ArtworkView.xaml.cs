c28ea914855f01ad2b5393cf07e573c61cdec45c
jiang miu miu <qewr367@yeah.net>
2026-08-28 15:19:30 +0800

    Remove original: src/AFMediaBar.Components.Wpf/BuiltIn/Artwork/ArtworkView.xaml.cs

--- src/AFMediaBar.Components.Wpf/BuiltIn/Artwork/ArtworkView.xaml.cs
+++
@@ -1,8 +0,0 @@
-using System.Windows.Controls;
-
-namespace AFMediaBar.Components.Wpf.BuiltIn.Artwork;
-
-public partial class ArtworkView : UserControl
-{
-    public ArtworkView() => InitializeComponent();
-}
