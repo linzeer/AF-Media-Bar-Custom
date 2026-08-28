94095c7bf561203b43877e9df736e1049d90caa4
jiang miu miu <qewr367@yeah.net>
2026-08-28 15:39:06 +0800

    Remove: src/AFMediaBar.Components/BuiltIn/Media/MediaSourceSettings.cs

--- src/AFMediaBar.Components/BuiltIn/Media/MediaSourceSettings.cs
+++
@@ -1,8 +0,0 @@
-using AFMediaBar.Components.Abstractions;
-
-namespace AFMediaBar.Components.BuiltIn.Media;
-
-public sealed record MediaSourceSettings(int FontSizeDip = 11, int MaxLines = 1) : IComponentSettings
-{
-    public string TypeId => ComponentTypeIds.MediaSource;
-}
