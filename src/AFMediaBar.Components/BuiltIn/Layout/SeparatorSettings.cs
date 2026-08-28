2d5885b9512bdcafdfd78035df1e304deecd1a4f
jiang miu miu <qewr367@yeah.net>
2026-08-28 15:38:56 +0800

    Remove: src/AFMediaBar.Components/BuiltIn/Layout/SeparatorSettings.cs

--- src/AFMediaBar.Components/BuiltIn/Layout/SeparatorSettings.cs
+++
@@ -1,8 +0,0 @@
-using AFMediaBar.Components.Abstractions;
-
-namespace AFMediaBar.Components.BuiltIn.Layout;
-
-public sealed record SeparatorSettings(int ThicknessDip = 1, int LengthDip = 22) : IComponentSettings
-{
-    public string TypeId => ComponentTypeIds.Separator;
-}
