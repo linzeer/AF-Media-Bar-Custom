bb620003647958bfebca7c6e43eba5b90939e512
jiang miu miu <qewr367@yeah.net>
2026-08-28 15:20:37 +0800

    Archive original: src/AFMediaBar.Components.Wpf/BuiltIn/Separator/SeparatorView.xaml.cs

---
+++ archive-original/src/AFMediaBar.Components.Wpf/BuiltIn/Separator/SeparatorView.xaml.cs
@@ -0,0 +1,8 @@
+using System.Windows.Controls;
+
+namespace AFMediaBar.Components.Wpf.BuiltIn.Separator;
+
+public partial class SeparatorView : UserControl
+{
+    public SeparatorView() => InitializeComponent();
+}
