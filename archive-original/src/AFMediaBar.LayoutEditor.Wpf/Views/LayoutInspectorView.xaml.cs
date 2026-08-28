6288b83399b13095e6e63c0d63bdcfe7a2db48f7
jiang miu miu <qewr367@yeah.net>
2026-08-28 15:27:44 +0800

    Archive: src/AFMediaBar.LayoutEditor.Wpf/Views/LayoutInspectorView.xaml.cs

---
+++ archive-original/src/AFMediaBar.LayoutEditor.Wpf/Views/LayoutInspectorView.xaml.cs
@@ -0,0 +1,8 @@
+using System.Windows.Controls;
+
+namespace AFMediaBar.LayoutEditor.Wpf.Views;
+
+public partial class LayoutInspectorView : UserControl
+{
+    public LayoutInspectorView() => InitializeComponent();
+}
