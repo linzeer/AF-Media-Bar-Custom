9023f87e6d6703f83e72877c513068d3a2dc7494
jiang miu miu <qewr367@yeah.net>
2026-08-28 15:19:24 +0800

    Archive original: src/AFMediaBar.Components.Wpf/BuiltIn/Artwork/ArtworkView.xaml.cs

---
+++ archive-original/src/AFMediaBar.Components.Wpf/BuiltIn/Artwork/ArtworkView.xaml.cs
@@ -0,0 +1,8 @@
+using System.Windows.Controls;
+
+namespace AFMediaBar.Components.Wpf.BuiltIn.Artwork;
+
+public partial class ArtworkView : UserControl
+{
+    public ArtworkView() => InitializeComponent();
+}
