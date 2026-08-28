6bb3f8661e4794fb5275a4a398dcb39e77477e66
jiang miu miu <qewr367@yeah.net>
2026-08-28 15:43:14 +0800

    Remove: src/AFMediaBar.LayoutEditor.Wpf/Views/LayoutEditorView.xaml.cs

--- src/AFMediaBar.LayoutEditor.Wpf/Views/LayoutEditorView.xaml.cs
+++
@@ -1,8 +0,0 @@
-using System.Windows.Controls;
-
-namespace AFMediaBar.LayoutEditor.Wpf.Views;
-
-public partial class LayoutEditorView : UserControl
-{
-    public LayoutEditorView() => InitializeComponent();
-}
