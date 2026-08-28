4312c4eeeeab7acfb8cf6f2206942d4858e3921d
jiang miu miu <qewr367@yeah.net>
2026-08-28 15:27:40 +0800

    Archive: src/AFMediaBar.LayoutEditor.Wpf/Views/LayoutEditorView.xaml.cs

---
+++ archive-original/src/AFMediaBar.LayoutEditor.Wpf/Views/LayoutEditorView.xaml.cs
@@ -0,0 +1,8 @@
+using System.Windows.Controls;
+
+namespace AFMediaBar.LayoutEditor.Wpf.Views;
+
+public partial class LayoutEditorView : UserControl
+{
+    public LayoutEditorView() => InitializeComponent();
+}
