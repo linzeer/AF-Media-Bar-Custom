e8dd8c5bfcf179ada03c9fe0f17fd76d69969669
jiang miu miu <qewr367@yeah.net>
2026-08-28 15:38:21 +0800

    Remove: src/AFMediaBar.Components/BuiltIn/Audio/OutputDeviceSettings.cs

--- src/AFMediaBar.Components/BuiltIn/Audio/OutputDeviceSettings.cs
+++
@@ -1,8 +0,0 @@
-using AFMediaBar.Components.Abstractions;
-
-namespace AFMediaBar.Components.BuiltIn.Audio;
-
-public sealed record OutputDeviceSettings(int ButtonSizeDip = 24) : IComponentSettings
-{
-    public string TypeId => ComponentTypeIds.OutputDevice;
-}
