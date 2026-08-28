109b923314747485090881524354a07f6d54b9f5
jiang miu miu <qewr367@yeah.net>
2026-08-28 15:39:30 +0800

    Remove: src/AFMediaBar.Core/Abstractions/IArtworkUriLoader.cs

--- src/AFMediaBar.Core/Abstractions/IArtworkUriLoader.cs
+++
@@ -1,9 +0,0 @@
-namespace AFMediaBar.Abstractions;
-
-/// <summary>
-/// Loads artwork from a remote URI and decodes it with the active UI shell.
-/// </summary>
-public interface IArtworkUriLoader
-{
-    Task<ArtworkDecodeResult> LoadAsync(Uri uri, CancellationToken cancellationToken);
-}
