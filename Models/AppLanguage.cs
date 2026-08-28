1a70ad9fec047ce14f74ccb96544b51fe154c598
jiang miu miu <qewr367@yeah.net>
2026-08-28 15:35:39 +0800

    Add: Models/AppLanguage.cs

---
+++ Models/AppLanguage.cs
@@ -0,0 +1,12 @@
+namespace AFMediaBar.Models;
+
+/// <summary>
+/// 界面语言。FollowSystem 跟随 Windows 显示语言，其余为用户显式选择。
+/// </summary>
+internal enum AppLanguage
+{
+    FollowSystem = 0,
+    ZhCn = 1,
+    ZhTw = 2,
+    EnUs = 3
+}
