192fd6b7627e945a8438ccb1485c2d6dfd4fcd14
jiang miu miu <qewr367@yeah.net>
2026-08-28 15:46:31 +0800

    Remove: src/AFMediaBar/ViewModels/ViewModelBase.cs

--- src/AFMediaBar/ViewModels/ViewModelBase.cs
+++
@@ -1,8 +0,0 @@
-using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace AFMediaBar.ViewModels;
-
-/// <summary>Shared notification base for WPF presentation state.</summary>
-public abstract class ViewModelBase : ObservableObject
-{
-}
