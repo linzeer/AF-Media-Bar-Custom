61a3e7efce34b79baeb2d19fabb809b1d17bd234
jiang miu miu <qewr367@yeah.net>
2026-08-28 15:40:16 +0800

    Remove: src/AFMediaBar.Core/Models/SystemMetricsSnapshot.cs

--- src/AFMediaBar.Core/Models/SystemMetricsSnapshot.cs
+++
@@ -1,7 +0,0 @@
-namespace AFMediaBar.Models;
-
-public readonly record struct SystemMetricsSnapshot(
-    int SystemMemoryPercent,
-    int? SystemCpuPercent,
-    int? SystemGpuPercent,
-    long ProcessMemoryMegabytes);
