5af5bcd6690d3a3ef99dc01c4b483c5657704ea7
jiang miu miu <qewr367@yeah.net>
2026-08-28 15:20:25 +0800

    Archive original: src/AFMediaBar.Components.Wpf/BuiltIn/PlaybackCommand/PlaybackCommandView.xaml.cs

---
+++ archive-original/src/AFMediaBar.Components.Wpf/BuiltIn/PlaybackCommand/PlaybackCommandView.xaml.cs
@@ -0,0 +1,8 @@
+using System.Windows.Controls;
+
+namespace AFMediaBar.Components.Wpf.BuiltIn.PlaybackCommand;
+
+public partial class PlaybackCommandView : UserControl
+{
+    public PlaybackCommandView() => InitializeComponent();
+}
