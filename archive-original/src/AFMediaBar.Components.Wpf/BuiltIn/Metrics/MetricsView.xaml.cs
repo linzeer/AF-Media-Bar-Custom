3acb9937f8564cac53f8f570ed9ec443af555a42
jiang miu miu <qewr367@yeah.net>
2026-08-28 15:20:00 +0800

    Archive original: src/AFMediaBar.Components.Wpf/BuiltIn/Metrics/MetricsView.xaml.cs

---
+++ archive-original/src/AFMediaBar.Components.Wpf/BuiltIn/Metrics/MetricsView.xaml.cs
@@ -0,0 +1,8 @@
+using System.Windows.Controls;
+
+namespace AFMediaBar.Components.Wpf.BuiltIn.Metrics;
+
+public partial class MetricsView : UserControl
+{
+    public MetricsView() => InitializeComponent();
+}
