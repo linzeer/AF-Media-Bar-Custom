afc88920cb6120e055d24c29bb27c3798714f622
jiang miu miu <qewr367@yeah.net>
2026-08-28 15:20:31 +0800

    Remove original: src/AFMediaBar.Components.Wpf/BuiltIn/PlaybackCommand/PlaybackCommandView.xaml.cs

--- src/AFMediaBar.Components.Wpf/BuiltIn/PlaybackCommand/PlaybackCommandView.xaml.cs
+++
@@ -1,8 +0,0 @@
-using System.Windows.Controls;
-
-namespace AFMediaBar.Components.Wpf.BuiltIn.PlaybackCommand;
-
-public partial class PlaybackCommandView : UserControl
-{
-    public PlaybackCommandView() => InitializeComponent();
-}
