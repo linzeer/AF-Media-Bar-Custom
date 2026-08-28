72db376ad0688f4dc7629f4e73a792264c93058c
jiang miu miu <qewr367@yeah.net>
2026-08-28 15:19:48 +0800

    Archive original: src/AFMediaBar.Components.Wpf/BuiltIn/MediaText/MediaTextView.xaml.cs

---
+++ archive-original/src/AFMediaBar.Components.Wpf/BuiltIn/MediaText/MediaTextView.xaml.cs
@@ -0,0 +1,8 @@
+using System.Windows.Controls;
+
+namespace AFMediaBar.Components.Wpf.BuiltIn.MediaText;
+
+public partial class MediaTextView : UserControl
+{
+    public MediaTextView() => InitializeComponent();
+}
