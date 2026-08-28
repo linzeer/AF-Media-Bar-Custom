757aa931f51a9cec56cf2eb60f2824c9090f7319
jiang miu miu <qewr367@yeah.net>
2026-08-28 15:19:42 +0800

    Remove original: src/AFMediaBar.Components.Wpf/BuiltIn/MediaSource/MediaSourceView.xaml.cs

--- src/AFMediaBar.Components.Wpf/BuiltIn/MediaSource/MediaSourceView.xaml.cs
+++
@@ -1,8 +0,0 @@
-using System.Windows.Controls;
-
-namespace AFMediaBar.Components.Wpf.BuiltIn.MediaSource;
-
-public partial class MediaSourceView : UserControl
-{
-    public MediaSourceView() => InitializeComponent();
-}
